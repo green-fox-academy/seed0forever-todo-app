2922403013620044d136358807d63a4bd90c83ab
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-06 13:03:17 +0200

    implement Print usage

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 public class App {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
+    try {
+      Path usagePath = Paths.get("usage.txt");
+      List<String> usageLines = Files.readAllLines(usagePath);
+      for (String line : usageLines) {
+        System.out.println(line);
+      }
+    } catch (IOException e) {
+      System.out.println("Could not read usage.txt file.");
+      e.printStackTrace();
+    }
   }
 
 }
